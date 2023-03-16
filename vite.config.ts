b82a1a9be1f1d1199ab1bf3261bdd489ced3ff8b
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-03-16 20:25:45 +0800

    添加 Pinia、alias 配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,37 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath, URL } from 'url'
+import { loadEnv } from "vite";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({mode}) => defineConfig({
   plugins: [
     vue(),
     Components({
       resolvers: [AntDesignVueResolver()],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+    cors: true,
+    proxy: {
+      '/dev-api': {
+        target: loadEnv(mode, process.cwd()).VITE_APP_SERVER_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/dev-api/, ''),
+      },
+      '/prod-api': {
+        target: loadEnv(mode, process.cwd()).VITE_APP_SERVER_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/prod-api/, ''),
+      },
+    },
+  },
 })
